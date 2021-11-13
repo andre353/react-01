b84b33d55645e3acc45c59e9bf8ba13ae8785428
Elena Andreeva <elena@madebyforward.com>
2021-11-13 18:09:57 +0300

    Add 2 buttons: add new expense and cancel functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import Card from '../UI/Card';
+import ExpensesList from "../ExpensesFilter/ExpensesList";
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -15,21 +15,10 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === pickedYear;
     });
 
-    let expensesContent = <p>No expenses found.</p>;
-
-    if(filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.length > 0 && filteredExpenses.map((item) => (<ExpenseItem
-            key={item.id}
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-        /> ))
-    }
-
     return(
         <Card className="expenses">
             <ExpensesFilter selected={pickedYear} onChangeFilter={filterChangeHandler} />
-            {expensesContent}
+            <ExpensesList items={filteredExpenses}/>
         </Card>
     );
 }
